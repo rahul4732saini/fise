39600d346f30757851f094c57666608efa3130eb
rahul4732saini <rahul4732saini@gmail.com>
2024-05-02 21:41:36 +0530

    Added file documentation

---
+++ fise/common/constants.py
@@ -0,0 +1,7 @@
+r"""
+Constants Module
+----------------
+
+This module comprises all the constants used throughout utility and
+are designed to assist other functionalities present withing it.
+"""

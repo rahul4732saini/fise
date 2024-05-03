a21be62d96026fe23ac5933ea472f506709a018f
rahul4732saini <rahul4732saini@gmail.com>
2024-05-03 16:59:26 +0530

    Added file documentation

---
+++ fise/operators/dataquery.py
@@ -0,0 +1,7 @@
+r"""
+Filequery Module
+----------------
+
+This module defines the FileQuery class used for performing
+all text search related operations within files.
+"""
---
+++ fise/operators/filequery.py
@@ -0,0 +1,7 @@
+r"""
+Filequery Module
+----------------
+
+This module defines the FileQuery class used for
+performing all file search related operations.
+"""

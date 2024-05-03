2937440b07e6c9edc0bf6bdaf37d5be40d919648
rahul4732saini <rahul4732saini@gmail.com>
2024-05-03 17:15:15 +0530

    Update fileparser.py
    Fixed an minor grammatical error in file
    documentation `defined` -> `defines`.

--- fise/parsers/fileparser.py
+++ fise/parsers/fileparser.py
@@ -2,6 +2,6 @@ r"""
 FileParser Module
 ----------------
 
-This module defined the FileParser class used for parsing user
+This module defines the FileParser class used for parsing user
 specified file search commands and performing related operations.
 """

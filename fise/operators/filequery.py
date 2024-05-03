e88f988b2ab4cf33235048f12098f35dc6c7f03f
rahul4732saini <rahul4732saini@gmail.com>
2024-05-03 18:01:36 +0530

    Defined `FileQuery.__init__` method

--- fise/operators/filequery.py
+++ fise/operators/filequery.py
@@ -6,10 +6,14 @@ This module defines the FileQuery class used for
 performing all file search related operations.
 """
 
+from pathlib import Path
+
 
 class FileQuery:
     r"""
     FileQuery defines methods used for
     performing all file search operations.
     """
-    
+
+    def __init__(self, directory: Path) -> None:
+        self._directory = directory

e49f9327c5c7e8c21063bd94a4272a0a047d3c36
rahul4732saini <rahul4732saini@gmail.com>
2024-05-03 17:14:11 +0530

    Fixed file documentation

--- fise/operators/dataquery.py
+++ fise/operators/dataquery.py
@@ -1,7 +1,7 @@
 r"""
-Filequery Module
+Dataquery Module
 ----------------
 
-This module defines the FileQuery class used for performing
-all text search related operations within files.
+This module defines the DataQuery class used for performing
+all data search related operations within files.
 """

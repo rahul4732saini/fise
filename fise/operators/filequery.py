059aa1b0207665eddc5ef2b190a93a85fc5ac812
rahul4732saini <rahul4732saini@gmail.com>
2024-05-03 19:04:05 +0530

    Update filequery.py
    Defined `FileQuery._get_files` method for extracting files from
    the specified directory and its sub-directories.

--- fise/operators/filequery.py
+++ fise/operators/filequery.py
@@ -6,6 +6,7 @@ This module defines the FileQuery class used for
 performing all file search related operations.
 """
 
+from typing import Generator
 from pathlib import Path
 
 
@@ -20,3 +21,19 @@ class FileQuery:
     def __init__(self, directory: Path, recursive: bool = False) -> None:
         self._directory = directory
         self._recursive = recursive
+
+    def _get_files(self, directory: Path) -> Generator[Path, None, None]:
+        r"""
+        Returns a `typing.Generator` object of all files present within the
+        specified directory. Also extracts the files present within the
+        sub-directories if `self._recursive` is set `True` at initialization.
+        """
+
+        for i in directory.glob('*'):
+            if i.is_file():
+                yield i
+
+            # Extracts files from sub-directories.
+            elif self._recursive and i.is_dir():
+                for i in self._get_files(i):
+                    yield i

42f7081f800c6d8a11d5c8ced528e2d059da7506
rahul4732saini <rahul4732saini@gmail.com>
2024-05-05 22:25:07 +0530

    Declared `File` class in `objects.py`
    Declared `File` class and added class documentation.

--- fise/objects.py
+++ fise/objects.py
@@ -5,3 +5,10 @@ Objects Module
 This module comprises classes that serve as foundational components
 for various objects and functionalities within the project.
 """
+
+
+class File:
+    r"""
+    File class serves as a unified class for accessing all methods related to the
+    file `pathlib.Path` and `os.stat_result` object methods and attributes.
+    """

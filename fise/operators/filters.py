393122dcd014d59fd8d1fc72f7f50aaa9feeb5a2
rahul4732saini <rahul4732saini@gmail.com>
2024-05-07 12:46:27 +0530

    Declared `FileSearchFilter` class

--- fise/operators/filters.py
+++ fise/operators/filters.py
@@ -5,3 +5,9 @@ Filters Module
 This Module is designed to facilitate objects and methods for filtration
 of search data results based on the specified user queries.
 """
+
+
+class FileSearchFilter:
+    r"""
+    FileSearchFilter class comprises methods for filtering file search results.
+    """

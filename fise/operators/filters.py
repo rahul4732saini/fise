e452acaf35bd337c366b4f834ca193b5eb7cd2ca
rahul4732saini <rahul4732saini@gmail.com>
2024-05-07 14:32:38 +0530

    Updated `FileSearchFilter` class in `operators/filters.py`
    `FileSearchFilter` class inherited from `BaseFilter` class. Also removed
    boiler-plate code in `FileSearchFilter` class.

--- fise/operators/filters.py
+++ fise/operators/filters.py
@@ -29,23 +29,10 @@ class BaseFilter:
         return self._data
 
 
-class FileSearchFilter:
+class FileSearchFilter(BaseFilter):
     r"""
     FileSearchFilter class comprises methods for filtering file search results.
     """
-
-    def __init__(self, data: pd.DataFrame) -> None:
-        r"""
-        Creates an instance of the FileSearchFilter class.
-
-        #### Params:
-        - data (pd.DataFrame): pandas DataFrame containing search results.
-        """
-        self._data = data
-
-    @property
-    def data(self) -> pd.DataFrame:
-        return self._data
 
     def by_filename(self, name: str):
         r"""

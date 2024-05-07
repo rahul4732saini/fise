124b828e9b8f22023e78838a757aa7944e834592
rahul4732saini <rahul4732saini@gmail.com>
2024-05-07 12:58:50 +0530

    Defined `FileSearchFilter.__init__` method

--- fise/operators/filters.py
+++ fise/operators/filters.py
@@ -6,8 +6,19 @@ This Module is designed to facilitate objects and methods for filtration
 of search data results based on the specified user queries.
 """
 
+import pandas as pd
+
 
 class FileSearchFilter:
     r"""
     FileSearchFilter class comprises methods for filtering file search results.
     """
+
+    def __init__(self, data: pd.DataFrame) -> None:
+        r"""
+        Creates an instance of the FileSearchFilter class.
+
+        #### Params:
+        - data (pd.DataFrame): pandas DataFrame containing search results.
+        """
+        self._data = data

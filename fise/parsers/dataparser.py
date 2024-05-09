826a91194abc8da5b941573a1039e1091593d6e8
rahul4732saini <rahul4732saini@gmail.com>
2024-05-09 21:27:44 +0530

    Merged `fileparser.py` and `dataparser.py` into `parsers.py`

--- fise/parsers/dataparser.py
+++
@@ -1,7 +0,0 @@
-r"""
-DataParser Module
-----------------
-
-This module defines the DataParser class used for parsing user
-specified data search commands and performing related operations.
-"""
--- fise/parsers/fileparser.py
+++
@@ -1,7 +0,0 @@
-r"""
-FileParser Module
-----------------
-
-This module defines the FileParser class used for parsing user
-specified file search commands and performing related operations.
-"""

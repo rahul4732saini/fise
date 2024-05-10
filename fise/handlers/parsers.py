6ca6f141790b8066787d3c57f5ef0d8064825f76
rahul4732saini <rahul4732saini@gmail.com>
2024-05-10 14:35:51 +0530

    Declared `FileQueryParser` class in `handlers/parsers.py`

--- fise/handlers/parsers.py
+++ fise/handlers/parsers.py
@@ -5,3 +5,10 @@ Parsers Module
 This modules comprises objects and methods for parsing user
 queries extracting relavent data for further processing.
 """
+
+
+class FileQueryParser:
+    r"""
+    FileQueryParsers defined methods for parsing
+    file search/manipulation operation queries.
+    """

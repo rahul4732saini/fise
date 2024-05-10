70cd8127b10a69f994fca29ad4a1c2242b8f41af
rahul4732saini <rahul4732saini@gmail.com>
2024-05-10 15:54:52 +0530

    Added `FileQueryParser.__init__` method

--- fise/handlers/parsers.py
+++ fise/handlers/parsers.py
@@ -6,9 +6,24 @@ This modules comprises objects and methods for parsing user
 queries extracting relavent data for further processing.
 """
 
+from typing import Literal
+
 
 class FileQueryParser:
     r"""
     FileQueryParsers defined methods for parsing
     file search/manipulation operation queries.
     """
+
+    def __init__(
+        self, query: list[str], operation: Literal["select", "remove"]
+    ) -> None:
+        r"""
+        Creates an instance of `FileQueryParser` class.
+
+        #### Params:
+        - query (list[str]): query to be parsed.
+        - operation ['select' | 'remove']: the operation to be performed upon the query.
+        """
+        self._query = query
+        self._operation = operation

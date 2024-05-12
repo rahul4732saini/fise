e1386560eb3597608301057a39e9213ba7befd64
rahul4732saini <rahul4732saini@gmail.com>
2024-05-12 13:39:19 +0530

    Added file documentation in `fise/handlers/__init__.py`

---
+++ fise/handlers/__init__.py
@@ -0,0 +1,7 @@
+r"""
+Handlers Package
+----------------
+
+This package provides a collection of objects and methods designed for
+parsing and processing user-specified search and manipulation queries.
+"""

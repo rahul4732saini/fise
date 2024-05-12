01d794a88fcb1f8592fb7e572439949c521602da
rahul4732saini <rahul4732saini@gmail.com>
2024-05-12 11:35:23 +0530

    Defined `QueryParseError` class in `errors.py`

--- fise/errors.py
+++ fise/errors.py
@@ -5,3 +5,19 @@ Errors Module
 This module defines error classes used throughout the
 utility to handle various exceptional scenarios.
 """
+
+import sys
+
+
+class QueryParseError:
+    r"""
+    Exception raised when there is an error parsing the query.
+    """
+
+    def __init__(self, description: str) -> None:
+        print(
+            "QueryParseError: Unable to parse the query.",
+            f"Description: {description}",
+            sep="\n",
+        )
+        sys.exit(1)

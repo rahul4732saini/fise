2900b907811ffa584c86229e9c2ccc476687ec1a
rahul4732saini <rahul4732saini@gmail.com>
2024-05-12 23:10:18 +0530

    Defined `QueryHandler._parse_export_data` method

--- fise/handlers/__init__.py
+++ fise/handlers/__init__.py
@@ -6,6 +6,14 @@ This package provides a collection of objects and methods designed for
 parsing and processing user-specified search and manipulation queries.
 """
 
+import re
+from pathlib import Path
+
+import pandas as pd
+
+from ..common import constants
+from ..errors import QueryParseError
+from ..shared import QueryInitials, ExportData
 from .parsers import FileQueryParser, FileDataQueryParser, DirectoryQueryParser
 from .operators import FileQueryOperator, FileDataQueryOperator, DirectoryQueryOperator
 
@@ -23,3 +31,30 @@ class QueryHandler:
         Creates an instance of the `QueryHandler` class.
         """
         self._query = query
+
+    @staticmethod
+    def _parse_export_data(query: list[str]) -> ExportData | None:
+        r"""
+        Parses export data from the query if specified else returns `None`.
+        """
+
+        if query[0].lower() != "export":
+            return
+        
+        if query[1].lower().startswith("sql"):
+            if not re.match(rf"^SQL\[({"|".join(constants.DATABASES)})\]$", query[1]):
+                QueryParseError(
+                    "Unable to parse SQL database name."
+                )
+
+            return ExportData("database", query[1][4:-1])
+        
+        else:
+            path: Path = Path(query[1])
+
+            if not path.is_file():
+                QueryParseError(
+                    "The path specified for exporting search records must be an existing file."
+                )
+
+            return ExportData("file", path)

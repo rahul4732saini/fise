a724c138512358ef60d388dcba49931a57a1eaec
rahul4732saini <rahul4732saini@gmail.com>
2024-05-19 16:01:19 +0530

    Reformatted `SIZE_CONVERSION_MAP` constant in `constants.py`

--- fise/common/constants.py
+++ fise/common/constants.py
@@ -42,24 +42,9 @@ SIZE_UNITS = Literal[
 # Mapping of storage unit string labels mapped with corresponding divisors
 # for storage size conversion into specified units.
 SIZE_CONVERSION_MAP = {
-    "b": 0.125,
-    "B": 1,
-    "Kb": 125,
-    "KB": 1e3,
-    "Kib": 128,
-    "KiB": 1024,
-    "Mb": 1.25e5,
-    "MB": 1e6,
-    "Mib": 131_072,
-    "MiB": 1024**2,
-    "Gb": 1.25e8,
-    "GB": 1e9,
-    "Gib": 134_217_728,
-    "GiB": 1024**3,
-    "Tb": 1.25e11,
-    "TB": 1e12,
-    "Tib": 137_438_953_472,
-    "TiB": 1024**4,
+    "b": 0.125, "B": 1, "Kb": 125, "KB": 1e3, "Kib": 128, "KiB": 1024, "Mb": 1.25e5,
+    "MB": 1e6, "Mib": 131_072, "MiB": 1024**2, "Gb": 1.25e8, "GB": 1e9, "Gib": 134_217_728,
+    "GiB": 1024**3, "Tb": 1.25e11, "TB": 1e12, "Tib": 137_438_953_472, "TiB": 1024**4
 }
 
 # Mapping of file suffixes mapped with corresponding

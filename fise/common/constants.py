d22b525a3fcbe083e5579c9b213914ac76c080b6
rahul4732saini <rahul4732saini@gmail.com>
2024-05-22 22:11:59 +0530

    Updated `common/constants.py`
    Converted constant `DATABASES` into a `typing.Literal` object.

--- fise/common/constants.py
+++ fise/common/constants.py
@@ -75,7 +75,7 @@ DIR_FIELD_ALIASES = {"perms": "permissions"}
 PATH_TYPES = {"absolute", "relative"}
 
 # Supported databases for data export.
-DATABASES = "postgresql", "mysql", "sqlite"
+DATABASES = Literal["postgresql", "mysql", "sqlite"]
 
 DATABASE_URL_DIALECTS = {
     "postgresql": "postgresql://",

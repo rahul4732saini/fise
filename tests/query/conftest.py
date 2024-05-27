778bbfd890c69c83df2d34e63e6ff937f492c64a
rahul4732saini <rahul4732saini@gmail.com>
2024-05-27 23:04:49 +0530

    Update `tests/query/conftest.py`
    Defined new fixtures for search fields in `tests/query/conftest.py`.

--- tests/query/conftest.py
+++ tests/query/conftest.py
@@ -11,3 +11,25 @@ sys.path.append(str(Path(__file__).parents[2] / "fise"))
 @pytest.fixture
 def test_directory() -> Path:
     return Path(__file__).parents[1] / "test_directory"
+
+
+@pytest.fixture
+def file_fields() -> tuple[str, ...]:
+    posix_fields: tuple = ("owner", "group") if sys.platform != "win32" else ()
+
+    return (
+        ("name", "path", "parent", "permissions")
+        + posix_fields
+        + ("size", "filetype", "access_time", "create_time", "modify_time")
+    )
+
+
+@pytest.fixture
+def data_fields() -> tuple[str, ...]:
+    return "name", "path", "lineno", "data"
+
+
+@pytest.fixture
+def dir_fields() -> tuple[str, ...]:
+    posix_fields: tuple = ("owner", "group") if sys.platform != "win32" else ()
+    return ("name", "path", "parent", "permissions") + posix_fields

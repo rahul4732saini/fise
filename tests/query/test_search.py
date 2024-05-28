2705ca82436c5bc7de072b9b50282b93026c329b
rahul4732saini <rahul4732saini@gmail.com>
2024-05-28 16:49:56 +0530

    Update `tests/query/test_search.py`
    Defined tests for directory search query in `TestDirSearchQuery` class in `tests/query/test_search.py`.

--- tests/query/test_search.py
+++ tests/query/test_search.py
@@ -19,10 +19,12 @@ class TestFileSearch:
     Tests the file search query with different test cases.
     """
 
+    # fmt: off
     _size_units = (
     "b", "B", "Kb", "KB", "Kib", "KiB", "Mb", "MB", "Mib",
     "MiB", "Gb", "GB", "Gib", "GiB", "Tb", "TB", "Tib", "TiB"
     )
+    # fmt: on
 
     @staticmethod
     def test_basic_search_query_syntax(test_directory: Path) -> None:
@@ -137,3 +139,133 @@ class TestFileSearch:
             r"(NAME IN ('main.py', 'array.py', 'random.py') OR NAME LIKE '^.*\.js$') AND "
             "MTIME BETWEEN ('2022-12-07', '2024-07-03')"
         )
+
+
+class TestDirSearchQuery:
+    """
+    Tests the data search query with different test cases.
+    """
+
+    @staticmethod
+    def test_basic_query_syntax(test_directory: Path) -> None:
+        """
+        Tests basic directory search query syntax with uppercase and lowercase characters.
+        """
+        _handle_query(f"select[type dir] * from '{test_directory}'")
+        _handle_query(f"SELECT[TYPE DIR] * FROM '{test_directory}'")
+
+    @staticmethod
+    def test_individual_search_fields(
+        test_directory: Path, dir_fields: tuple[str, ...]
+    ) -> None:
+        """Tests the directory search query with individual fields."""
+
+        for field in dir_fields:
+            _handle_query(f"select[type dir] {field} from '{test_directory}'")
+            _handle_query(f"select[type dir] {field.upper()} from '{test_directory}'")
+
+    @staticmethod
+    def test_multiple_search_field_patterns(
+        test_directory: Path, dir_fields: tuple[str, ...]
+    ) -> None:
+        """Tests the directory search query with multiple field patterns."""
+
+        dir_fields += ("*",)
+
+        for fields in (
+            random.choices(dir_fields, k=random.choice(range(1, 5))) for _ in range(5)
+        ):
+            _handle_query(
+                f"select[type dir] {','.join(fields)} from '{test_directory}'"
+            )
+            _handle_query(
+                f"select[type dir] {', '.join(fields)} from '{test_directory}'"
+            )
+
+    @staticmethod
+    def test_recursive_search(test_directory: Path) -> None:
+        """Tests the recursive operator in directory search query."""
+
+        for i in ("r", "recursive", "R", "RECURSIVE"):
+            _handle_query(f"{i} select * from '{test_directory}'")
+
+    @staticmethod
+    def test_search_with_different_path_types(test_directory: Path) -> None:
+        """Tests the directory search query with different path types."""
+
+        for type_ in ("absolute", "relative", "ABSOLUTE", "RELATIVE"):
+            _handle_query(f"r select[type dir] * from {type_} '{test_directory}'")
+
+    @staticmethod
+    def test_export_to_file(
+        test_directory: Path, test_export_files: tuple[Path, ...]
+    ) -> None:
+        """Tests exporting directory search records to different file types."""
+
+        for file in test_export_files:
+            _handle_query(f"export '{file}' select[type dir] * from '{test_directory}'")
+
+            # Verifies whether the export was successful.
+            assert file.exists()
+            file.unlink()
+
+    @staticmethod
+    def test_search_conditions_with_comparison_operators(test_directory: Path) -> None:
+        """Tests the directory search query conditions with comparison operators."""
+
+        _handle_query(
+            f"select[type dir] * from '{test_directory}' where name = 'Software'"
+        )
+        _handle_query(
+            f"SELECT[TYPE DIR] * FROM '{test_directory}' WHERE NAME = "
+            "'Media' OR NAME = 'Archive' AND PERMS = 16395"
+        )
+
+        if sys.platform != "win32":
+            _handle_query(
+                f"select[type dir] * from '{test_directory}' where name = "
+                "'Documents' and owner = 'none' and group != 'unknown'"
+            )
+
+    @staticmethod
+    def test_search_conditions_with_conditional_operators(test_directory: Path) -> None:
+        """Tests the directroy search query conditions with conditional operators."""
+
+        _handle_query(
+            f"select[type dir] * from '{test_directory}' where name like "
+            "'^(Documents|Software)$' and path like '^.*/test_directory/.*$' "
+            "and parent in ('test_directory', 'tests', 'archive')"
+        )
+        _handle_query(
+            f"SELECT[TYPE DIR] * FROM '{test_directory}' WHERE NAME IN ('Software',"
+            " 'Documents', 'Archive') AND PARENT LIKE '^.*/test_directory'"
+        )
+
+    @staticmethod
+    def test_nested_search_conditions(test_directory: Path) -> None:
+        """Tests the directory search query with nested conditions."""
+
+        _handle_query(
+            f"select[type dir] * from '{test_directory}' where (name ="
+            " 'Software' and parent like '^.*/test_directory')"
+        )
+        _handle_query(
+            f"SELECT[TYPE DIR] * FROM '{test_directory}' WHERE (PATH LIKE '^.*/Documents/.*$'"
+            " OR PATH LIKE '^.*/Software/.*$') AND NAME IN ('Libraries', 'Meeting_Notes')"
+        )
+
+    @staticmethod
+    def miscellaneous_tests(
+        test_directory: Path, test_export_files: tuple[Path, ...]
+    ) -> None:
+        """Tests various aspects of the directory search query."""
+
+        for file in test_export_files:
+            _handle_query(
+                f"EXPORT '{file}' r SELECT * from ABSOLUTE '{test_directory}' WHERE name IN "
+                "('Sofware', 'Libraries', 'Documents', 'Music') OR path like '^.*(Media|Archive)'"
+            )
+
+            # Verifies whether the export was successful.
+            assert file.exists()
+            file.unlink()

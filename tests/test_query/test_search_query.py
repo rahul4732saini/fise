025c8dbdb9148fa34a1c55dbb26ea9c8d02d270f
rahul4732saini <rahul4732saini@gmail.com>
2024-07-01 22:32:27 +0530

    Defined test cases in test_search_query.py

---
+++ tests/test_query/test_search_query.py
@@ -0,0 +1,37 @@
+"""
+This module comprises test cases for verifying
+the functionality of search queries in FiSE.
+"""
+
+from pathlib import Path
+
+import pytest
+import pandas as pd
+
+from fise.query import QueryHandler
+
+TEST_DIRECTORY = Path(__file__).parents[1] / "test_directory"
+
+
+class TestFileSearchQuery:
+    """
+    Tests the file search queries.
+    """
+
+    basic_query_syntax_params = [
+        f"SELECT * FROM '{TEST_DIRECTORY / 'file_dir'}'",
+        f"SELECT[TYPE FILE] name, filetype FROM '{TEST_DIRECTORY / 'file_dir'}'",
+        f"RECURSIVE SELECT * FROM '{TEST_DIRECTORY}' WHERE filetype = '.py'",
+    ]
+
+    @pytest.mark.parametrize("query", basic_query_syntax_params)
+    def test_basic_query_syntax(self, query: str) -> None:
+        """
+        Tests the basic file search query syntax.
+        """
+
+        # This test doesn't explicitly verifies the extracted data is flexible and
+        # subject to change depending on the system and path it's executed from.
+
+        data: pd.DataFrame = QueryHandler(query).handle()
+        assert isinstance(data, pd.DataFrame)

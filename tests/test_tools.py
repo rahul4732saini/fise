f9826ecbf6409199965b06a71edaeb03cf290f2f
rahul4732saini <rahul4732saini@gmail.com>
2024-07-01 21:49:57 +0530

    Update test_tools.py
    Defined `test_parse_query_function` test function in test_tools.py

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -11,7 +11,14 @@ from fise.common import tools
 
 TEST_DIRECTORY = Path(__file__).parent / "test_directory"
 
-# Test parameters defined below.
+# Test parameters for individual functions defined below
+
+PARSE_QUERY_FUNC_PARAMS = [
+    r"SELECT[TYPE FILE] * FROM . WHERE name LIKE '^.*\.py$' AND ctime BETWEEN ('2020-01-01', '2022-01-01')",
+    "DELETE[TYPE DIR] FROM '/home/user/Projects 2020' WHERE 'temp' IN name",
+    "SELECT[TYPE DATA, MODE BYTES] lineno, dataline FROM ./fise/main.py WHERE '#TODO' IN dataline",
+    "SELECT path, size FROM ./documents WHERE filetype='.docx' AND (ctime < '2022-01-01' OR ctime > '2023-12-31')",
+]
 
 GET_FILES_FUNC_PARAMS = [
     (1, TEST_DIRECTORY, True),
@@ -25,12 +32,46 @@ GET_DIRS_FUNC_PARAMS = [
     (3, TEST_DIRECTORY / "file_dir/reports", True),
 ]
 
+# Test results for individual functions defined below
+
+PARSE_QUERY_TEST_RESULTS = [
+    [
+        "SELECT[TYPE FILE]", "*", "FROM", ".", "WHERE",
+        "name", "LIKE", r"'^.*\.py$'", "AND", "ctime",
+        "BETWEEN", "('2020-01-01', '2022-01-01')",
+    ],
+    [
+        "DELETE[TYPE DIR]", "FROM",
+        "'/home/user/Projects 2020'",
+        "WHERE", "'temp'", "IN", "name",
+    ],
+    [
+        "SELECT[TYPE DATA, MODE BYTES]", "lineno,",
+        "dataline", "FROM", "./fise/main.py", "WHERE",
+        "'#TODO'", "IN", "dataline",
+    ],
+    [
+        "SELECT", "path,", "size", "FROM", "./documents",
+        "WHERE", "filetype='.docx'", "AND",
+        "(ctime < '2022-01-01' OR ctime > '2023-12-31')",
+    ],
+]
+
 
 def read_tests_hdf(path: str) -> pd.Series | pd.DataFrame:
     """Reads test data from `test_tools.hdf` file."""
 
     with pd.HDFStore(Path(__file__).parent / "test_tools.hdf") as store:
         return store[path]
+
+
+@pytest.mark.parametrize(
+    ("query", "result"), zip(PARSE_QUERY_FUNC_PARAMS, PARSE_QUERY_TEST_RESULTS)
+)
+def test_parse_quest_function(query: str, result: list[str]) -> None:
+    """Tests the `tools.parse_query` function."""
+    parsed_query: list[str] = tools.parse_query(query)
+    assert parsed_query == result
 
 
 @pytest.mark.parametrize(("ctr", "path", "recur"), GET_FILES_FUNC_PARAMS)

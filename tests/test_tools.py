1bd08cc3929a137ede745ed3d461b1aa74c7839a
rahul4732saini <rahul4732saini@gmail.com>
2024-07-01 21:11:58 +0530

    Update test_tools.py
    Renamed function parameter constant names in test_tools.py

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -13,13 +13,13 @@ TEST_DIRECTORY = Path(__file__).parent / "test_directory"
 
 # Test parameters defined below.
 
-TEST_GET_FILES_FUNC_PARAMS = [
+GET_FILES_FUNC_PARAMS = [
     (1, TEST_DIRECTORY, True),
     (2, TEST_DIRECTORY / "file_dir", False),
     (3, TEST_DIRECTORY / "file_dir/docs", True),
 ]
 
-TEST_GET_DIRS_FUNC_PARAMS = [
+GET_DIRS_FUNC_PARAMS = [
     (1, TEST_DIRECTORY, True),
     (2, TEST_DIRECTORY / "file_dir", False),
     (3, TEST_DIRECTORY / "file_dir/reports", True),
@@ -33,14 +33,14 @@ def read_tests_hdf(path: str) -> pd.Series | pd.DataFrame:
         return store[path]
 
 
-@pytest.mark.parametrize(("ctr", "path", "recur"), TEST_GET_FILES_FUNC_PARAMS)
+@pytest.mark.parametrize(("ctr", "path", "recur"), GET_FILES_FUNC_PARAMS)
 def test_get_files_function(ctr: int, path: Path, recur: bool) -> None:
     """Tests the `get_files` function."""
     files: pd.Series = pd.Series(str(path) for path in tools.get_files(path, recur))
     assert files.equals(read_tests_hdf(f"/function/get_files/test{ctr}"))
 
 
-@pytest.mark.parametrize(("ctr", "path", "recur"), TEST_GET_DIRS_FUNC_PARAMS)
+@pytest.mark.parametrize(("ctr", "path", "recur"), GET_DIRS_FUNC_PARAMS)
 def test_get_directories_function(ctr: int, path: Path, recur: bool) -> None:
     """Tests the `get_files` function."""
     files: pd.Series = pd.Series(

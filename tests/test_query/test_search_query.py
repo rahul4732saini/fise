7792bd2fa20e3391c6a6070bbb8ef1049de02e41
rahul4732saini <rahul4732saini@gmail.com>
2024-07-04 10:14:11 +0530

    Update test_search_query.py
    Defined `test_mixed_case_query` method in `TestFileSearchQuery` class
    for testing file search queries comprising characters of mixed case.

--- tests/test_query/test_search_query.py
+++ tests/test_query/test_search_query.py
@@ -24,10 +24,32 @@ class TestFileSearchQuery:
         f"RECURSIVE SELECT * FROM '{TEST_DIRECTORY}' WHERE filetype = '.py'",
     ]
 
+    mixed_case_query_params = [
+        f"r Select * FroM '{TEST_DIRECTORY / 'file_dir'}'",
+        f"sELect[TYPE FILE] Name, FileType From '{TEST_DIRECTORY / 'file_dir'}'",
+        f"RecURSive sELECt * From '{TEST_DIRECTORY}' wHErE FilETypE = '.py'",
+    ]
+
     @pytest.mark.parametrize("query", basic_query_syntax_params)
     def test_basic_query_syntax(self, query: str) -> None:
         """
         Tests the basic file search query syntax.
+        """
+
+        # This test doesn't explicitly verifies the extracted data as it is flexible
+        # and subject to change depending on the system and path it's executed from.
+
+        data: pd.DataFrame = QueryHandler(query).handle()
+        assert isinstance(data, pd.DataFrame)
+
+    # The following test uses the same delete queries defined for basic query syntax test
+    # comprising characters of mixed cases, and hence uses the file and directory records
+    # stored at the same path in the `test_delete_query.hdf` file.
+    
+    @pytest.mark.parametrize("query", mixed_case_query_params)
+    def test_mixed_case_query(self, query: str) -> None:
+        """
+        Tests file search queries comprising characters of mixed cases.
         """
 
         # This test doesn't explicitly verifies the extracted data as it is flexible

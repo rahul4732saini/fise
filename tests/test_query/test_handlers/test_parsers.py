33d0033cd9b128fe7eedb34d94c319b150fef3ea
rahul4732saini <rahul4732saini@gmail.com>
2024-07-15 17:08:40 +0530

    Define new test case in test_parsers.py
    Defined `test_search_query_with_field_aliases` method in `TestDirectoryQueryParser`
    class for testing directory query parser with search query comprising field aliases.

--- tests/test_query/test_handlers/test_parsers.py
+++ tests/test_query/test_handlers/test_parsers.py
@@ -157,6 +157,12 @@ class TestDirectoryQueryParser:
         "* FROM ABSOLUTE '.' WHERE atime >= '2024-02-20'",
     ]
 
+    search_query_with_field_aliases_test_params = [
+        "filename, filepath FROM ABSOLUTE .",
+        "filepath, ctime,atime FROM . WHERE atime <= '2023-01-01' AND ctime >= '2006-02-20'",
+        "filename, mtime FROM RELATIVE . WHERE mtime BETWEEN ('2021-03-12', '2021-04-12')",
+    ]
+
     # The following list comprises results for the file search query test comprising sub-lists,
     # each with a length of 2. The first element of each sub-list signifies whether the path is
     # absolute (True) or relative (False) whereas the second element is a list comprising names
@@ -169,6 +175,12 @@ class TestDirectoryQueryParser:
         [True, list(constants.DIR_FIELDS)],
     ]
 
+    search_query_with_field_aliases_test_results = [
+        [True, ["name", "path"], ["filename", "filepath"]],
+        [False, ["path", "create_time", "access_time"], ["filepath", "ctime", "atime"]],
+        [False, ["name", "modify_time"], ["filename", "mtime"]],
+    ]
+
     @pytest.mark.parametrize(
         ("subquery", "results"),
         zip(search_query_test_params, search_query_test_results),
@@ -178,6 +190,26 @@ class TestDirectoryQueryParser:
 
         query: list[str] = tools.parse_query(subquery)
         parser = DirectoryQueryParser(query, "search")
+
+        search_query: SearchQuery = examine_search_query(parser, results)
+        fields: list[str] = results[1]
+
+        assert [field.field for field in search_query.fields] == fields
+
+    @pytest.mark.parametrize(
+        ("subquery", "results"),
+        zip(
+            search_query_with_field_aliases_test_params,
+            search_query_with_field_aliases_test_results,
+        ),
+    )
+    def test_search_query_with_field_aliases(self, subquery, results) -> None:
+        """
+        Tests the file query parser with search queries comprising field aliases.
+        """
+
+        query: list[str] = tools.parse_query(subquery)
+        parser = FileQueryParser(query, "search")
 
         search_query: SearchQuery = examine_search_query(parser, results)
         fields: list[str] = results[1]

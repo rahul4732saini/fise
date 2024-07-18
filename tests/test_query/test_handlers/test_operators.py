07b553b9df8f6eee6442b7ecc6c9ebb9902738b9
rahul4732saini <rahul4732saini@gmail.com>
2024-07-18 15:18:45 +0530

    Update test_operators.py
    Defined mechanism for testing methods defined within `FileQueryOperator` class.

---
+++ tests/test_query/test_handlers/test_operators.py
@@ -0,0 +1,76 @@
+"""
+This module comprises test cases for verifying
+the functionality of operator classes in FiSE.
+"""
+
+from pathlib import Path
+from typing import Callable
+
+import pytest
+import pandas as pd
+
+from fise.shared import File, Field
+from fise.query.operators import FileQueryOperator
+
+
+FILE_DIR_TEST_DIRECTORY = Path(__file__).parents[2] / "test_directory/file_dir"
+TEST_RECORDS_FILE = Path(__file__).parent / "test_operators.hdf"
+
+
+def read_hdf(file: str, path: str) -> pd.Series:
+    """Reads records stored at the path mentioned from specified HDF5 file."""
+
+    with pd.HDFStore(file) as store:
+        return store[path]
+
+
+def verify_search_operation(path: str, data: pd.DataFrame) -> None:
+    """
+    Verifies the pandas dataframe extracted from the search operation with the
+    records stored at the specified path in the `test_operators.hdf` file.
+    """
+    global TEST_RECORDS_FILE
+
+    assert isinstance(data, pd.DataFrame)
+
+    results: pd.DataFrame = read_hdf(TEST_RECORDS_FILE, path)
+    assert data.equals(results)
+
+
+class TestFileQueryOperator:
+    """Tests the FileQueryOperator class"""
+
+    def condition1(file: File) -> bool:
+        return file.filetype is not None
+
+    def condition2(file: File) -> bool:
+        return not file.size
+
+    def condition3(file: File) -> bool:
+        return file.name in ("unknown.mp3", "runaway.mp3", "birthday.avi")
+
+    search_operation_test_params = [
+        (1, Path(FILE_DIR_TEST_DIRECTORY), False, ["name", "filetype"], condition1),
+        (2, Path(FILE_DIR_TEST_DIRECTORY / "reports"), True, ["name"], condition2),
+    ]
+
+    @pytest.mark.parametrize(
+        ("index", "directory", "recursive", "columns", "condition"),
+        search_operation_test_params,
+    )
+    def test_search_operation(
+        self,
+        index: int,
+        directory: Path,
+        recursive: bool,
+        columns: list[str],
+        condition: Callable[[File], bool],
+    ) -> None:
+        """Tests file query operator with the search operation."""
+
+        fields: list[Field] = [Field(name) for name in columns]
+
+        operator = FileQueryOperator(directory, recursive)
+        data: pd.DataFrame = operator.get_dataframe(fields, columns, condition)
+
+        verify_search_operation(f"/file/search/test{index}", data)

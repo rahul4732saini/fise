913f720f3d46bc7a6496c1c63d7162565a8cf86d
rahul4732saini <rahul4732saini@gmail.com>
2024-07-24 21:36:35 +0530

    Update conftest.py

--- tests/test_query/test_handlers/conftest.py
+++ tests/test_query/test_handlers/conftest.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 
 ADDITIONAL_PATHS = [
+    str(Path(__file__).parents[1]),
     str(Path(__file__).parents[2]),
     str(Path(__file__).parents[3]),
     str(Path(__file__).parents[3] / "fise"),

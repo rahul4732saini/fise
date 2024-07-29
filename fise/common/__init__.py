28fdc7f732cc63419bb3800c8730debec60ad507
rahul4732saini <rahul4732saini@gmail.com>
2024-07-29 20:05:52 +0530

    Update documentation in common/__init__.py

--- fise/common/__init__.py
+++ fise/common/__init__.py
@@ -2,8 +2,8 @@
 Common package
 --------------
 
-This packages comprises constants and utility functions
-assisting others classes and functions throughout the project.
+This packages comprises constants and utility functions designed
+for assisting others classes and functions throughout the project.
 """
 
 __all__ = "constants", "tools"

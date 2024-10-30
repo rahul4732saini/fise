de5323953daf4025ad2b74ed2516a48fce6a3c1d
rahul4732saini <rahul4732saini@gmail.com>
2024-10-30 13:15:03 +0530

    Refactor documentation in notify.py

--- fise/notify.py
+++ fise/notify.py
@@ -3,19 +3,19 @@ Notify Module
 -------------
 
 This module comprises class for displaying alerts
-and notifications on the command-line interface.
+and notifications on the Command Line Interface.
 """
 
 
 class Message:
-    """Prints a success message on the terminal window."""
+    """Prints a success message on the Command Line Interface."""
 
     def __init__(self, mesg: str) -> None:
         print(f"\033[32m{mesg}\033[0m")
 
 
 class Alert:
-    """Prints an alert message on the terminal window."""
+    """Prints an alert message on the Command Line Interface."""
 
     def __init__(self, mesg: str) -> None:
         print(f"\033[33m{mesg}\033[0m")

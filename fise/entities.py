ebf0fa52598c7b30272ee8a8d5c135dd6a75c173
rahul4732saini <rahul4732saini@gmail.com>
2024-10-31 15:18:16 +0530

    Update entities.py
    Defined the `get_datetime` function in entities.py to convert a
    POSIX timestamp into a `datetime.datetime` object.

--- fise/entities.py
+++ fise/entities.py
@@ -6,7 +6,11 @@ This module comprises classes and functions for
 storing and handling file system metadata fields.
 """
 
+import os
 from typing import Callable, Any
+from pathlib import Path
+from datetime import datetime
+
 from notify import Alert
 
 
@@ -41,5 +45,51 @@ def safe_extract_field(func: Callable[..., Any]) -> Callable[..., Any]:
     return wrapper
 
 
+def get_datetime(timestamp: float) -> datetime:
+    """Constructs a `datetime.datetime` object from a POSIX timestamp."""
+    return datetime.fromtimestamp(timestamp).replace(microsecond=0)
+
+
 class BaseEntity:
     """BaseEntity serves as the base class for all other entity classes."""
+
+
+class FileSystemEntity(BaseEntity):
+    """
+    FileSystemEntity serves as the base class
+    for all file system entity classes.
+    """
+
+    def __init__(self, path: Path) -> None:
+        self._path: Path = path
+        self._stats: os.stat_result = path.stat()
+
+    @property
+    @safe_extract_field
+    def name(self) -> str:
+        return self._path.name
+
+    @property
+    @safe_extract_field
+    def path(self) -> str:
+        return self._path.as_posix()
+
+    @property
+    @safe_extract_field
+    def parent(self) -> str:
+        return self._path.parent.as_posix()
+
+    @property
+    @safe_extract_field
+    def access_time(self) -> datetime:
+        return get_datetime(self._stats.st_atime)
+
+    @property
+    @safe_extract_field
+    def create_time(self) -> datetime:
+        return get_datetime(self._stats.st_ctime)
+
+    @property
+    @safe_extract_field
+    def modify_time(self) -> datetime:
+        return get_datetime(self._stats.st_mtime)

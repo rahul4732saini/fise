0cbc224a5a95b1b242d0884704e8a37fee8e902a
rahul4732saini <rahul4732saini@gmail.com>
2024-10-31 16:52:18 +0530

    Remove redundant code in shared.py
    Removed redundant field classes in shared.py as already moved to
    fields.py

--- fise/shared.py
+++ fise/shared.py
@@ -6,97 +6,13 @@ This module comprises data-classes shared across the project
 assisting various other classes and functions defined within it.
 """
 
-import re
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import ClassVar, Callable, Literal, Any
+from typing import Callable, Literal, Any
 from pathlib import Path
 
 from common import constants
-from errors import QueryParseError
-from entities import BaseEntity, File
-
-
-@dataclass(slots=True, frozen=True, eq=False)
-class AbstractField(ABC):
-    """AbstractField serves as the abstract class for all field classes."""
-
-    @classmethod
-    @abstractmethod
-    def parse(cls): ...
-
-    @abstractmethod
-    def evaluate(self, entity: BaseEntity) -> Any: ...
-
-
-@dataclass(slots=True, frozen=True, eq=False)
-class Field(AbstractField):
-    """
-    Field class stores individual query fields and
-    defines methods for parsing and evaluating them.
-    """
-
-    field: str
-
-    @classmethod
-    def parse(cls, field: str):
-        """
-        Parses the specified field and returns
-        an instance of the `Field` class.
-        """
-        return cls(field)
-
-    def evaluate(self, entity: BaseEntity) -> Any:
-        """
-        Evaluates the stored field object based on associated
-        attributes within the specified entity object.
-        """
-        return getattr(entity, self.field)
-
-
-@dataclass(slots=True, frozen=True, eq=False)
-class Size(AbstractField):
-    """
-    Size class stores the size conversion divisor for converting
-    file sizes and defines methods for parsing and evaluating them.
-    """
-
-    # Regex pattern for matching size field specifications.
-    _size_field_pattern: ClassVar[re.Pattern] = re.compile(r"^size(\[.*])?$")
-
-    divisor: int
-
-    @classmethod
-    def parse(cls, field: str):
-        """
-        Parses the specified field specifications
-        and creates an instance of the `Size` class.
-        """
-
-        if not cls._size_field_pattern.match(field.lower()):
-            raise QueryParseError(f"Found an invalid field {field!r} in the query.")
-
-        # Assigns "B" -> bytes unit is not explicitly specified.
-        unit: str = field[5:-1] or "B"
-        divisor: int | None = constants.SIZE_CONVERSION_MAP.get(unit)
-
-        if divisor is None:
-            raise QueryParseError(
-                f"Invalid unit {unit!r} specified for the 'size' field."
-            )
-
-        return cls(divisor)
-
-    def evaluate(self, file: File) -> float | None:
-        """
-        Extracts the size from the specified `File` object and
-        converts it in accordance with the stored size divisor.
-        """
-
-        if file.size is None:
-            return None
-
-        return round(file.size / self.divisor, 5)
+from entities import BaseEntity
+from fields import BaseField
 
 
 @dataclass(slots=True, frozen=True, eq=False)
@@ -111,7 +27,7 @@ class BaseQuery:
 class SearchQuery(BaseQuery):
     """SearchQuery class stores search query attributes."""
 
-    fields: list[Field | Size]
+    fields: list[BaseField]
     columns: list[str]
 
 

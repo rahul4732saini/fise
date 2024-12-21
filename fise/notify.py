56a4177ced96883266b9e897480c97ed9c3501a8
rahul4732saini <rahul4732saini@gmail.com>
2024-12-21 19:17:40 +0530

    Update notify.py
    Updated print statements setting the flush parameter to True in
    the Message and Alert class to flush data to the standard output
    immediately to avoid any delays in output.

--- fise/notify.py
+++ fise/notify.py
@@ -11,11 +11,11 @@ class Message:
     """Prints a success message on the Command Line Interface."""
 
     def __init__(self, mesg: str) -> None:
-        print(f"\033[32m{mesg}\033[0m")
+        print(f"\033[32m{mesg}\033[0m", flush=True)
 
 
 class Alert:
     """Prints an alert message on the Command Line Interface."""
 
     def __init__(self, mesg: str) -> None:
-        print(f"\033[33m{mesg}\033[0m")
+        print(f"\033[33m{mesg}\033[0m", flush=True)

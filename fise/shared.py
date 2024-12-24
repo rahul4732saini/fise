736cc819d92f22618d55936c5ecc10e9895e1c4e
rahul4732saini <rahul4732saini@gmail.com>
2024-12-24 15:50:24 +0530

    Define QueryQueue class in shared.py
    Defined `QueryQueue` class implementing the queue data structure
    for storing individual tokens of the user-specified query.

--- fise/shared.py
+++ fise/shared.py
@@ -10,9 +10,7 @@ from dataclasses import dataclass
 from typing import Iterable, Optional, Generator
 from pathlib import Path
 
-from common import constants
-from entities import BaseEntity
-from fields import BaseField
+from common import tools
 
 
 @dataclass(slots=True)
@@ -27,17 +25,74 @@ class QueueNode:
     next: Optional["QueueNode"] = None
 
 
-@dataclass(slots=True, frozen=True, eq=False)
-class SearchQuery(Query):
-    """SearchQuery class stores search query attributes."""
+class QueryQueue:
+    """
+    QueryQueue class implements the queue data structure for
+    storing individual tokens of the user-specified query.
+    """
 
-    fields: list[BaseField]
-    columns: list[str]
+    __slots__ = "_head", "_tail"
 
+    def __init__(self, iterable: Iterable | None = None) -> None:
+        """
+        Creates an instance of the QueryQueue class.
 
-class DeleteQuery(Query):
-    """DeleteQuery class stores delete query attributes."""
+        #### Params:
+        - itertable (Iterable | None): [OPTIONAL] Iterable for
+        initializing the queue. Defaults to None.
+        """
 
+        self._head = self._tail = None
+
+        if iterable is not None:
+            self._from_iterable(iterable)
+
+    def __bool__(self) -> bool:
+        return self._head is not None
+
+    def __repr__(self) -> str:
+        return f"QueryQueue(head={self._head.val!r})"
+
+    @classmethod
+    def from_string(cls, query: str) -> "QueryQueue":
+        """Initializes the queue from the specified query string."""
+
+        query: Generator[str, None, None] = tools.tokenize(query, skip_empty=True)
+        return cls(query)
+
+    def _from_iterable(self, iterable: Iterable) -> None:
+        """Adds the tokens in the specified iterable to the queue."""
+
+        for val in iterable:
+            self.add(val)
+
+    def add(self, token: str) -> None:
+        """Adds the specified token at the end of the queue."""
+
+        if self._head is None:
+            self._head = self._tail = QueueNode(token)
+            return
+
+        self._tail.next = QueueNode(token)
+        self._tail = self._tail.next
+
+    def pop(self) -> str:
+        """Pops out the token at the start of the queue."""
+
+        if self._head is None:
+            raise RuntimeError("Query queue is empty.")
+
+        val: str = self._head.val
+        self._head = self._head.next
+
+        if self._head is None:
+            self._tail = None
+
+        return val
+
+    def seek(self) -> str:
+        """Returns the token at the start of the queue."""
+        return self._head.val
 
 @dataclass(slots=True, frozen=True, eq=False)
 class ExportData:

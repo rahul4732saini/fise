301cee38f8692becb2f158ad596a25b1c45443ef
rahul4732saini <rahul4732saini@gmail.com>
2024-12-24 15:27:00 +0530

    Define QueryPathParser class in paths.py
    Defined `QueryPathParser` class for parsing the path specifications
    defined within the user-specified query and extracting relavent details
    for further usage.

--- fise/query/paths.py
+++ fise/query/paths.py
@@ -134,3 +134,62 @@ class DataQueryPath(BaseQueryPath):
             yield self._path
 
         yield from tools.enumerate_files(self._path, recursive)
+
+
+class QueryPathParser:
+    """
+    QueryPathParser class defines methods for parsing
+    path specifications from the user-specified query.
+    """
+
+    __slots__ = "_query", "_entity"
+
+    # Maps entity names with their corresponding query path classes.
+    _path_classes: dict[str, Type[BaseQueryPath]] = {
+        constants.ENTITY_FILE: FileQueryPath,
+        constants.ENTITY_DATA: DataQueryPath,
+        constants.ENTITY_DIR: DirectoryQueryPath,
+    }
+
+    def __init__(self, query: QueryQueue, entity: str) -> None:
+        """
+        Creates an instance of the QueryPathParser class.
+
+        #### Params:
+        - query (QueryQueue): `QueryQueue` object comprising the query.
+        - entity (str): Name of the entity being operated upon.
+        """
+
+        self._query = query
+        self._entity = entity
+
+    def _parse_specifications(self) -> Path:
+        """Parses the path specifications defined within the query."""
+
+        is_absolute: bool = False
+
+        if self._query.seek() in constants.PATH_TYPES:
+            is_absolute = self._query.pop() == constants.PATH_ABSOLUTE
+
+        raw_path: str = self._query.pop()
+
+        # Strips off the leading and trailing quotes from
+        # the path specifications if explicitly specified.
+        if constants.STRING_PATTERN.match(raw_path):
+            raw_path = raw_path[1:-1]
+
+        path = Path(raw_path)
+
+        if is_absolute:
+            path = path.resolve()
+
+        return path
+
+    def parse(self) -> BaseQueryPath:
+        """
+        Parses the path specifications, validates the path
+        and returns a query path object for further usage.
+        """
+
+        path = self._parse_specifications()
+        return self._path_classes[self._entity](path)

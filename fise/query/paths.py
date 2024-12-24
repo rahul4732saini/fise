0bf900d3f444b73397f863a6f977c4702038f0a2
rahul4732saini <rahul4732saini@gmail.com>
2024-12-24 15:24:06 +0530

    Define FileQueryPath class in paths.py
    Defined `FileQueryPath` class for storing and
    handling directory paths for the file query.

--- fise/query/paths.py
+++ fise/query/paths.py
@@ -10,6 +10,9 @@ from typing import Generator
 from pathlib import Path
 from abc import ABC, abstractmethod
 
+from common import tools
+from errors import QueryParseError
+
 
 class BaseQueryPath(ABC):
     """BaseQueryPath serves as the base class for all query path classes."""
@@ -30,3 +33,34 @@ class BaseQueryPath(ABC):
 
     @abstractmethod
     def enumerate(self, recursive: bool) -> Generator[Path, None, None]: ...
+
+
+class FileQueryPath(BaseQueryPath):
+    """
+    FileQueryPath class defines mechanism for storing the directory path
+    specified within the query, and enumerating over files within the same.
+    """
+
+    __slots__ = ("_path",)
+
+    def __repr__(self) -> str:
+        return f"FileQueryPath(path={self._path.as_posix()!r})"
+
+    def _validate_path(self, path: Path) -> None:
+        """Validates the specified path for a file query."""
+
+        if not path.is_dir():
+            raise QueryParseError(
+                f"The specified path {self!r} does not lead to a directory!"
+            )
+
+    def enumerate(self, recursive: bool) -> Generator[Path, None, None]:
+        """
+        Enumerates over the files within the specified directory. Files within
+        sub-directories are also include if `recursive` is set to True.
+
+        #### Params:
+        - recursive (bool): Whether to include files from sub-directories.
+        """
+
+        yield from tools.enumerate_files(self._path, recursive)

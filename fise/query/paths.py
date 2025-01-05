59d57da4eb20043d07cef13f872b5832f350e9f9
rahul4732saini <rahul4732saini@gmail.com>
2025-01-05 21:40:06 +0530

    Fix a parser method in QueryPathPArser class
    Fixed the `_parse_specifications` method where the path type if
    existing was equilated without being converted into lowercase.

--- fise/query/paths.py
+++ fise/query/paths.py
@@ -171,7 +171,7 @@ class QueryPathParser:
 
         is_absolute: bool = False
 
-        if self._query.seek() in constants.PATH_TYPES:
+        if self._query.seek().lower() in constants.PATH_TYPES:
             is_absolute = self._query.pop() == constants.PATH_ABSOLUTE
 
         raw_path: str = self._query.pop()

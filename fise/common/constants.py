1fb1250a5f39520b6b41e47d7ff463c850f96fc5
rahul4732saini <rahul4732saini@gmail.com>
2025-01-17 20:48:51 +0530

    Fix a minor bug in constants.py
    Fixed the type for the `size` query field in the `FIELD_TYPES` dictionary.

--- fise/common/constants.py
+++ fise/common/constants.py
@@ -109,7 +109,7 @@ FIELD_TYPES = {
     "lineno": int,
     "dataline": str,
     "filetype": str,
-    "size": int | float,
+    "size": float,
     "create_time": datetime,
     "access_time": datetime,
     "modify_time": datetime,

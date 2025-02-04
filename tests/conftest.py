b774668f21c5cd7d496f5a7f2981f3aeb4c33701
rahul4732saini <rahul4732saini@gmail.com>
2025-02-04 12:18:00 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,11 @@ from pathlib import Path
 # Adds the directories comprising the software into the
 # system path for testing the associated functionalities.
 
+BASE_DIR = Path(__file__).parents[1]
+
 ADDITIONAL_PATHS = [
-    Path(__file__).parents[1].as_posix(),
-    (Path(__file__).parents[1] / "fise").as_posix(),
+    BASE_DIR.as_posix(),
+    BASE_DIR.joinpath("fise").as_posix(),
 ]
 
 sys.path.extend(ADDITIONAL_PATHS)
